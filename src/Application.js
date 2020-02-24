73dcae98dcbd81b3106529757885e8ee851d8fda
stephenau93 <stephenau93@gmail.com>
2020-02-24 00:19:55 -0800

    convert useState to useReducer, add action type for ADD_GRUDGE, FORGIVE_GRUDGE into reducer

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,22 +7,49 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
-const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+const GRUDGE_ADD = "GRUDGE_ADD";
+const GRUDGE_FORGIVE = "GRUDGE_FORGIVE";
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+const reducer = (state, action) => {
+  if (action.type === GRUDGE_ADD) {
+    return [action.payload, ...state];
+  };
+  if (action.type === GRUDGE_FORGIVE) {
+    return state.map(grudge => {
+      if (grudge.id !== id) return grudge;
+      return {...grudge, forgive: !grudge.forgiven}
+    });
+  };
+  return state;
+}
+
+
+const Application = () => {
+  const [grudges, dispatch] = useReducer(reducer, initialState);
+
+  const addGrudge = ({ person, reason }) => {
+    dispatch({
+      type: GRUDGE_ADD,
+      payload: {
+        person,
+        reason,
+        forgiven: false,
+        id: id()
+      },
+      meta: {},
+      errors: []
+    });
   };
 
   const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload: {
+        id
+      },
+      meta: {},
+      errors: []
+    });
   };
 
   return (
